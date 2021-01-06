e798c8bf801255331186d0731786de7325d2bbf1
datomo <dave.lengw@gmail.com>
2021-01-07 00:18:40 +0100

    started replacing gpio

--- src/gui.rs
+++ src/gui.rs
@@ -3,70 +3,7 @@ use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 
-use iced::{Align, Application, button, Button, Column, Command, Element, executor, ProgressBar, Sandbox, Settings, Text};
-
 use crate::workflow::SensorStatus;
-
-pub fn main() -> iced::Result {
-    Counter::run(Settings::default())
-}
-
-#[derive(Default)]
-struct Counter {
-    value: i32,
-    increment_button: button::State,
-    decrement_button: button::State,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    IncrementPressed,
-    DecrementPressed,
-}
-
-impl Application for Counter {
-    type Executor = executor::Default;
-    type Message = Message;
-    type Flags = ();
-
-    fn new(_flags: ()) -> (Self, Command<Message>) {
-        (Self::default(), Command::none())
-    }
-
-    fn title(&self) -> String {
-        String::from("Counter - Iced")
-    }
-
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 10;
-            }
-            Message::DecrementPressed => {
-                self.value -= 10;
-            }
-        }
-        Command::none()
-    }
-
-    fn view(&mut self) -> Element<Message> {
-        let progress_bar = ProgressBar::new(0.0..=100.0, self.value as f32);
-        Column::new()
-            .padding(20)
-            .align_items(Align::Center)
-            .push(
-                Button::new(&mut self.increment_button, Text::new("Increment"))
-                    .on_press(Message::IncrementPressed),
-            )
-            .push(Text::new(self.value.to_string()).size(50))
-            .push(progress_bar)
-            .push(
-                Button::new(&mut self.decrement_button, Text::new("Decrement"))
-                    .on_press(Message::DecrementPressed),
-            )
-            .into()
-    }
-}
 
 /// struct should hold all available ?sensors? and expose them to the GUI
 pub struct GuiManager {
@@ -79,11 +16,7 @@ impl GuiManager {
         let (sender, receiver) = channel::<SensorStatus>();
         let gui = GuiManager { sender, receiver: Arc::new(Mutex::new(receiver)) };
         gui.loop_debug();
-        if has_gui {
-            thread::spawn(|| {
-                Counter::run(Settings::default());
-            });
-        }
+        if has_gui {}
         println!("starting gui");
         gui
     }
@@ -95,7 +28,7 @@ impl GuiManager {
     fn loop_debug(&self) {
         let local_sender = self.receiver.clone();
         thread::spawn(move || loop {
-            let msg =  local_sender.lock().unwrap().recv().unwrap();
+            let msg = local_sender.lock().unwrap().recv().unwrap();
             println!("{}", msg);
         });
     }
--- src/hx711.rs
+++ src/hx711.rs
@@ -3,10 +3,11 @@ use std::thread;
 
 use gpio::{GpioIn, GpioOut, GpioValue};
 use gpio::sysfs::{SysFsGpioInput, SysFsGpioOutput};
+use sysfs_gpio::{Direction, Pin, Error};
 
 pub(crate) struct Hx711 {
-    pd_sck: gpio::sysfs::SysFsGpioOutput,
-    dout: gpio::sysfs::SysFsGpioInput,
+    pd_sck: Pin,
+    dout: Pin,
     gain: i32,
     reference: i32,
     offset: i32,
@@ -14,9 +15,13 @@ pub(crate) struct Hx711 {
 
 impl Hx711 {
     pub(crate) fn new(sck_pin: i32, dout_pin: i32, gain: i32) -> Self {
+        let sck = Pin::new(sck_pin as u64);
+        let d_out = Pin::new(dout_pin as u64);
+        sck.set_direction(Direction::Out);
+        d_out.set_direction(Direction::In);
         Hx711 {
-            pd_sck: gpio::sysfs::SysFsGpioOutput::open(sck_pin as u16).unwrap(),
-            dout: gpio::sysfs::SysFsGpioInput::open(dout_pin as u16).unwrap(),
+            pd_sck: sck,
+            dout: d_out,
             gain: match gain {
                 128 => 1,
                 64 => 3,
@@ -26,10 +31,6 @@ impl Hx711 {
             reference: 1,
             offset: 38_200,
         }
-    }
-
-    fn is_ready(mut self) -> bool {
-        self.dout.read_value().unwrap() == GpioValue::Low
     }
 
     pub(crate) fn read(&mut self) -> i32 {
@@ -46,8 +47,8 @@ impl Hx711 {
         // HX711 Channel and gain factor are set by number of bits read
         // after 24 data bits.
         for i in 0..self.gain {
-            self.pd_sck.set_high().unwrap();
-            self.pd_sck.set_low().unwrap();
+            self.pd_sck.set_value(1).unwrap();
+            self.pd_sck.set_value(0).unwrap();
         }
 
         value = ((data[0] as i32) << 16
@@ -58,28 +59,31 @@ impl Hx711 {
     }
 
     fn wait_ready(&mut self) {
-        while self.dout.read_value().unwrap() != GpioValue::Low {
+        while match self.dout.get_value() {
+            Ok(val) => val != 0,
+            Err(err) => {
+                println!("{}",err);
+                false
+            }
+        }  {
             thread::sleep(time::Duration::from_millis(1));
-        }
+        };
     }
 
     fn read_next_byte(&mut self) -> u8 {
-        let mut value = 0;
+        let mut value:u8 = 0;
 
         for _ in 0..8 {
             value <<= 1;
-            value |= self.read_next_bit() as u8;
+            value |= self.read_next_bit();
         }
         value
     }
 
-    fn read_next_bit(&mut self) -> i32 {
-        self.pd_sck.set_high().unwrap();
-        self.pd_sck.set_low().unwrap();
-        match self.dout.read_value().unwrap() == GpioValue::High {
-            true => 1,
-            false => 0
-        }
+    fn read_next_bit(&mut self) -> u8 {
+        self.pd_sck.set_value(1).unwrap();
+        self.pd_sck.set_value(0).unwrap();
+        self.dout.get_value().unwrap()
     }
 
     pub(crate) fn get_units(&mut self, times: i32) -> f32 {
@@ -95,10 +99,11 @@ impl Hx711 {
             return self.read();
         }
 
-        let mut sum: i32 = 0;
-        for _ in 0..times {
-            sum += &self.read();
+        let mut sum = 0;
+        for i in 0..times {
+            sum += *&self.read();
         };
+
         (sum / times) as i32
     }
 
@@ -124,14 +129,14 @@ impl Hx711 {
     }
 
     fn power_down(&mut self) {
-        self.pd_sck.set_low().unwrap();
-        self.pd_sck.set_high().unwrap();
+        self.pd_sck.set_value(0).unwrap();
+        self.pd_sck.set_value(1).unwrap();
 
         thread::sleep(time::Duration::from_nanos(100))
     }
 
     fn power_up(&mut self) {
-        self.pd_sck.set_low().unwrap();
+        self.pd_sck.set_value(0);
 
         thread::sleep(time::Duration::from_nanos(100))
     }
